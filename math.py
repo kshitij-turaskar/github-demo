eed961311c1f6e2fe068758b428f0c07b9240d51
kshitij-turaskar <51197601+kshitij-turaskar@users.noreply.github.com>
2021-08-12 12:32:12 +0530

    Adding math.py file to remote

---
+++ math.py
@@ -0,0 +1,15 @@
+# add implementation
+def add(x,y):
+    return (x+y);
+# subtract implementation rebase example on Bug456
+def subtract(x,y):
+    return (x-y);
+# multiply implementation rebase implementation on master
+def multiply(x,y):
+    return (x*y);
+# divide implementation version 1 and version 2
+def divide(x,y):
+     return (x/y);
+
+#master rebase conflict #rebase example Bug 789
+
